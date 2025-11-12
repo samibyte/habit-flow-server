afeebeb78ee2be039d5428b33cc0ce00ce8132e9
samibyte <samibyte.web@gmail.com>
2025-11-12 15:10:12 +0600

    feat: add endpoint to create a new habit

--- index.js
+++ index.js
@@ -94,6 +94,14 @@ async function run() {
       res.json(result);
     });
 
+    //Create habit
+    app.post("/api/v1/habits", async (req, res) => {
+      const newHabit = req.body;
+      newHabit.createdAt = new Date();
+      const result = await habitsColl.insertOne(newHabit);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
